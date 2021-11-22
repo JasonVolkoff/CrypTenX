04fcf74122083c6dc83ecda91d0fe66a7635ee69
Jason Volkoff <kashijason@gmail.com>
2021-11-21 22:18:09 -0800

    Updated descriptive comments

--- cryptenx/management/commands/data_miner.py
+++ cryptenx/management/commands/data_miner.py
@@ -6,7 +6,8 @@ from calendar import timegm
 from datetime import date, datetime, timedelta
 from config.settings import CW_PUBLIC_KEY
 
-# TODO: 
+# TODO: Add support for gathering data on other currencies
+
 class Command(BaseCommand):
     help = "Gathers crypto data n' stuff."
 
@@ -19,39 +20,38 @@ class Command(BaseCommand):
         currency = kwargs['currency']
         self.days = kwargs['days']
         self.get_timestamps()
-        try:
-            params = {
-                'after': self.stop_timestamp,
-                'before': self.oldest_timestamp,
-                'periods': [180],
-                'apikey': CW_PUBLIC_KEY
-            }
-            resp = requests.get(f'https://api.cryptowat.ch/markets/coinbase-pro/{currency}usd/ohlc', params=params)
-        
-            if resp.ok:
-                print("Gathering data from cryptowatch API")
-                data = resp.json()
-                result = data['result']['180']
-                try:
-                    for data_point in result:
-                        timestamp = data_point[0]
-                        datetime_obj_with_tz = datetime.fromtimestamp(timestamp, tz=timezone.utc)
-                        EtheriumData.objects.create(
-                            close_time = datetime_obj_with_tz,
-                            open_price = data_point[1],
-                            high_price = data_point[2],
-                            low_price = data_point[3],
-                            close_price = data_point[4],
-                            volume = data_point[5],
-                            quote_volume = data_point[6]
-                        )
-                    print("Done!")
-                except:
-                    raise CommandError('Failed to save to the database.')
-            else:
-                print("Somthing went wrong dude...")
-        except:
-            raise CommandError('Operation Failed')
+        # Params 'periods' indicates a 3 minute period (60 seconds * 3)
+        # This setting will call data in 3 minute intervals
+        params = {
+            'after': self.stop_timestamp,
+            'before': self.oldest_timestamp,
+            'periods': [180],
+            'apikey': CW_PUBLIC_KEY
+        }
+        resp = requests.get(f'https://api.cryptowat.ch/markets/coinbase-pro/{currency}usd/ohlc', params=params)
+            
+        if resp.ok:
+            print("Gathering data from cryptowatch API")
+            data = resp.json()
+            result = data['result']['180']
+            try:
+                for data_point in result:
+                    timestamp = data_point[0]
+                    datetime_obj_with_tz = datetime.fromtimestamp(timestamp, tz=timezone.utc)
+                    EtheriumData.objects.create(
+                        close_time = datetime_obj_with_tz,
+                        open_price = data_point[1],
+                        high_price = data_point[2],
+                        low_price = data_point[3],
+                        close_price = data_point[4],
+                        volume = data_point[5],
+                        quote_volume = data_point[6]
+                    )
+                print("Done!")
+            except:
+                raise CommandError("Error saving data.")
+        else:
+            raise CommandError("Invalid API request.")
 
     def get_timestamps(self):
         print("Converting timestamps to UTC POSIX strings...")
